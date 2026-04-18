97b972a00e28deaa585bdb21ee5c9157c1e5f5da
Saksham73023 <sakshampanwar902@gmail.com>
2026-04-18 14:08:13 +0530

    Simplify slide activation code
    Refactor slide and dot activation logic for conciseness.

--- Script.js
+++ Script.js
@@ -1,13 +1,15 @@
 const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
 
 let currentSlide = 0;
 
 function showSlide(index) {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+  slides.forEach((slide) => slide.classList.remove("active"));
+  dots.forEach((dot) => dot.classList.remove("active"));
 
   slides[index].classList.add("active");
+  dots[index].classList.add("active");
+
   currentSlide = index;
 }
 
@@ -16,10 +18,7 @@ function nextSlide() {
   showSlide(currentSlide);
 }
 
-// auto slide
 setInterval(nextSlide, 3000);
-}
-
 function startAutoSlide() {
   autoSlide = setInterval(nextSlide, 3500);
 }
