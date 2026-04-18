5e310a05f127d831164fdd2c631c3a8c8a02baf3
Saksham73023 <sakshampanwar902@gmail.com>
2026-04-18 14:01:21 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,23 +1,23 @@
 const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
 
 let currentSlide = 0;
-let autoSlide;
 
 function showSlide(index) {
-  slides.forEach((slide, i) => {
+  slides.forEach((slide) => {
     slide.classList.remove("active");
-    dots[i].classList.remove("active");
   });
 
   slides[index].classList.add("active");
-  dots[index].classList.add("active");
   currentSlide = index;
 }
 
 function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
+}
+
+// auto slide
+setInterval(nextSlide, 3000);
 }
 
 function startAutoSlide() {
